e6173dbc4bfa7023311065e11c548e3fd9c51e57
brunaalura <134618759+brunaalura@users.noreply.github.com>
2025-11-07 10:52:49 -0300

    carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,15 @@
 var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
 
-//Percorrer todos os botoes controladores
+// Percorre todos os botoes controladores
 btns.forEach(function(btn) {
-    btn.addEventListener('click', function()) {
+  btn.addEventListener('click', function() {
+    
 
+    // Remove classe 'ativo' dos outros botoes
+    btns.forEach(function(btnRemoveClass) {
+      btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo')
+    })
 
-        //Remover class 'ativo' dos outros botoes
-        btns.forEach(function(btnRemoveClass) {
-            btnRemoveClass.classList.remove('listaDeArtigos-slider-item-ativo')
-        })
-
-        this.classList.add('listaDeArtigo-slider-item--ativo')
-        })
-
+    this.classList.add('listaDeArtigos-slider-item--ativo')
+  })
+})
